a8c5f3f0ee1977e3c14e78949f6d4aa4b31d3212
ZainabImadulla <113566112+ZainabImadulla@users.noreply.github.com>
2022-12-27 19:50:27 -0500

    hi

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import companyLogo from "/Users/zainab/Desktop/trial/src/images/sbb_sqlogoclear.
 import { BsInstagram } from "react-icons/bs";
 function App() {
   return (
+  
     <BrowserRouter basename = "/trial">
       <Navbar className="top" expand = "lg" bg-abwhite>
       <Container>
@@ -19,11 +20,11 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className = "dropDown">
           <Nav className="me-auto justify-content-between w-25 ">
-            <Nav.Link href="/" className = "nav-item" class = "nav-item nav-link ">HOME</Nav.Link>
-            <Nav.Link href="/about" className = "nav-item" class = "nav-item nav-link " >ABOUT </Nav.Link>
+            <Nav.Link href="/trial/" className = "nav-item" class = "nav-item nav-link ">HOME</Nav.Link>
+            <Nav.Link href="/trial/about" className = "nav-item" class = "nav-item nav-link " >ABOUT </Nav.Link>
            
-            <Nav.Link href="/contact"  className = "nav-item" class = "nav-item nav-link" >CONTACT</Nav.Link>
-            <Nav.Link href="/products" className = "nav-item" class = "nav-item nav-link " >PRODUCTS</Nav.Link>
+            <Nav.Link href="/trial/contact"  className = "nav-item" class = "nav-item nav-link" >CONTACT</Nav.Link>
+            <Nav.Link href="/trial/products" className = "nav-item" class = "nav-item nav-link " >PRODUCTS</Nav.Link>
           
           </Nav>
         </Navbar.Collapse>
@@ -36,7 +37,7 @@ function App() {
       </Navbar>
       <Routes>
 
-        <Route exact path = "/trial" element = {<Home />}/>
+        <Route exact path = "/" element = {<Home />}/>
         <Route path = "/about" element = {<AboutUs />}/>
         <Route path = "/contact" element = {<ContactUs />}/>
         <Route path = "/products" element = {<Products />}/>
