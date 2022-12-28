f1d62a9be967950bebe3ea1168e03c29100bca63
ZainabImadulla <113566112+ZainabImadulla@users.noreply.github.com>
2022-12-27 20:46:17 -0500

    kys

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter basename = "/trial">
       <Navbar className="top" expand = "lg" bg-abwhite>
       <Container>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="/trial/">
             <img className = "app-logo" src = {companyLogo} alt = " Sweet Batter Bakes"  />
         </Navbar.Brand>
         
@@ -22,7 +22,6 @@ function App() {
           <Nav className="me-auto justify-content-between w-25 ">
             <Nav.Link href="/trial/" className = "nav-item" class = "nav-item nav-link ">HOME</Nav.Link>
             <Nav.Link href="/trial/about" className = "nav-item" class = "nav-item nav-link " >ABOUT </Nav.Link>
-           
             <Nav.Link href="/trial/contact"  className = "nav-item" class = "nav-item nav-link" >CONTACT</Nav.Link>
             <Nav.Link href="/trial/products" className = "nav-item" class = "nav-item nav-link " >PRODUCTS</Nav.Link>
           
